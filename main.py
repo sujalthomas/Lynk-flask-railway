09f74190e8d72666ff7af24a1baadb6594a9f9f6
Sujal Thomas Tatipelli <42236789+sujalthomas@users.noreply.github.com>
2023-08-26 03:44:26 -0700

    t1

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from flask import Flask, render_template
 import os
+from dotenv import load_dotenv
+from flask_bcrypt import Bcrypt
+from flask_session import Session
 
+
+
+# Load environment variables
+load_dotenv()
 
 app = Flask(__name__)
+
+
+bcrypt = Bcrypt(app)
+
+#initialize extensions
+sess = Session()
+sess.init_app(app)
+
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
